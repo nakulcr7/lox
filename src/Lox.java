ad5214743dfe23557c80fa227fe6d8e74366a735
Nakul Camasamudram <nakul.cr7@gmail.com>
2019-04-10 22:42:05 -0700

    scanner support for single character tokens and strings

--- src/Lox.java
+++ src/Lox.java
@@ -4,6 +4,7 @@ import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 
 public class Lox {
@@ -12,7 +13,7 @@ public class Lox {
 
   public static void main(String args[]) throws IOException {
     if (args.length > 1) {
-      System.out.println("Usage: lox [script]");
+      System.out.println("Usage: jlox [script]");
       System.exit(64);
     } else if (args.length == 1) {
       runFile(args[0]);
